46c2bd5bb9756f4a0a6213f470332476bf7bd3a6
Anton Kanakhin <anton.kanahin@lamoda.ru>
2026-03-09 22:55:34 +0400

    HW5 is completed

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,14 +2,78 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"slices"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
+type Counter struct {
+	counter int
+	mu      sync.Mutex
+}
+
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	taskSlice := tasks[:]
+	inProgressCount := Counter{}
+	errorCount := Counter{}
+	wg := &sync.WaitGroup{}
+
+	for len(taskSlice) > 0 && (errorCount.Get() < m || m <= 0) {
+		if inProgressCount.Get() >= n {
+			continue
+		}
+
+		task := taskSlice[0]
+		taskSlice = slices.Delete(taskSlice, 0, 1)
+
+		wg.Add(1)
+
+		inProgressCount.Inc()
+
+		go func(task Task) {
+			defer func() {
+				inProgressCount.Dec()
+
+				wg.Done()
+			}()
+
+			err := task()
+
+			if err != nil {
+				errorCount.Inc()
+			}
+		}(task)
+	}
+
+	wg.Wait()
+
+	if m > 0 && errorCount.Get() >= m {
+		return ErrErrorsLimitExceeded
+	}
+
 	return nil
+}
+
+func (c *Counter) Inc() {
+	c.mu.Lock()
+	c.counter++
+	c.mu.Unlock()
+}
+
+func (c *Counter) Dec() {
+	c.mu.Lock()
+	c.counter--
+	c.mu.Unlock()
+}
+
+func (c *Counter) Get() int {
+	defer c.mu.Unlock()
+
+	c.mu.Lock()
+
+	return c.counter
 }
